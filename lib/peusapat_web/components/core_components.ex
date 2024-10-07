1ae2e2121c30c35f20fe4b71eea2740bd8de8142
Riza Fahmi <rizafahmi@gmail.com>
2024-10-07 10:07:20 +0700

    Change theme to teal

--- lib/peusapat_web/components/core_components.ex
+++ lib/peusapat_web/components/core_components.ex
@@ -231,8 +231,8 @@ defmodule PeusapatWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "phx-submit-loading:opacity-75",
+        "text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
         @class
       ]}
       {@rest}
