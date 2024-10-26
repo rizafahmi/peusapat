05f046f6b4412e8fe4558588da4c90cdad8f1776
Riza Fahmi <rizafahmi@gmail.com>
2024-10-26 08:30:21 +0700

    Remove some files from gen auth

--- lib/peusapat_web/router.ex
+++ lib/peusapat_web/router.ex
@@ -60,7 +60,7 @@ defmodule PeusapatWeb.Router do
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{PeusapatWeb.UserAuth, :redirect_if_user_is_authenticated}] do
       # live "/users/register", UserRegistrationLive, :new
-      # live "/users/log_in", UserLoginLive, :new
+      live "/users/log_in", UserLoginLive, :new
       # live "/users/reset_password", UserForgotPasswordLive, :new
       # live "/users/reset_password/:token", UserResetPasswordLive, :edit
     end
